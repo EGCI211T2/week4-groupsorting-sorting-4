8094424195ee37f73109823ad38008bfc2121b62
Hail2You <125166920+Hail2You@users.noreply.github.com>
2025-10-21 11:26:00 +0700

    Update and rename main.c to main.cpp

---
+++ main.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <cstdlib>
+
+using namespace std;
+#include "sorting.h"
+
+
+
+int main(int argc, char * argv[]) {
+    
+ 
+  cout<<"argc is "<<argc<<endl;
+    int i,N;
+    
+    N=argc-1;
+    int* a = new int[N];
+
+    for(i=1;i<argc;i++){
+        cout<<"argv[" << i<<"]" <<argv[i]<<endl;
+      
+    }
+
+    /* Convert to numbers*/
+    for(int i=0;i<N;i++){
+      a[i]=atoi(argv[i+1]);
+    }
+    
+    cout<<"Before sorting:";
+    display(a,N);
+    
+    sort(a,N);
+    cout<<"After sorting:";
+    display(a,N);
+    
+    
+    delete []a;
+    return 0;
+}
